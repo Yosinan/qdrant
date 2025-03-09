cb44a1a9eb75647460c6cba083c1e1437093b782
Yosinan <ymymym4634@gmail.com>
2025-03-09 11:35:16 +0300

    update

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,8 +1,8 @@
-docker build -t gcr.io/astral-field-448905-v3/clinical-agent .
-docker push gcr.io/astral-field-448905-v3/clinical-agent
+docker build -t gcr.io/astral-field-448905-v3/clinical-agent-api .
+docker push gcr.io/astral-field-448905-v3/clinical-agent-api
 
-gcloud run deploy clinical-agent \
-  --image gcr.io/astral-field-448905-v3/clinical-agent \
+gcloud run deploy clinical-agent-api \
+  --image gcr.io/astral-field-448905-v3/clinical-agent-api \
   --platform managed \
   --region us-central1 \
   --allow-unauthenticated
